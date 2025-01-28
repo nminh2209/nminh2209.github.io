786da1bf4c0f87e2634fc541024741c8248a3198
Nguyen Minh <nminh22092209@gmail.com>
2025-01-28 10:59:32 +0700

    alottt
    changes

--- Client/script/FAQ.js
+++
@@ -1,8 +0,0 @@
-const accordionItems = document.querySelectorAll('.accordion-item');
-
-accordionItems.forEach(item => {
-    const heading = item.querySelector('.accordion-heading');
-    heading.addEventListener('click', () => {
-        item.classList.toggle('active');
-    });
-});
